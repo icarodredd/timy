e4ac8faaf041b0f6c16d52e8d2d5c31ffc2c4079
icarodredd <icarog854@gmail.com>
2025-09-19 10:32:03 -0300

    feat: add header

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import timyLogo from "@/public/timy-logo.png";
+import { Box, Button, Link as ChakraLink } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaUserCircle } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <Box
+      backdropFilter={"blur(8.7px)"}
+      boxShadow={"0 4px 30px rgba(0, 0, 0, 0.1)"}
+      backgroundColor={"rgba(255, 255, 255, 0.08)"}
+      as={"header"}
+      display={"flex"}
+      justifyContent={"space-between"}
+      py={8}
+    >
+      <Image src={timyLogo} alt="Timy Logo" width={200} height={200} />
+      <Box display={"flex"} gap={10}>
+        <ChakraLink asChild>
+          <NextLink href="/best-offers">Best Offers</NextLink>
+        </ChakraLink>
+        <ChakraLink asChild>
+          <NextLink href="/masculine">Masculine</NextLink>
+        </ChakraLink>
+        <ChakraLink asChild>
+          <NextLink href="/feminine">Feminine</NextLink>
+        </ChakraLink>
+      </Box>
+      <Box alignItems={"center"} display={"flex"} gap={6}>
+        <Button variant={"ghost"} asChild>
+          <a href="/cart">
+            <FaCartShopping />
+          </a>
+        </Button>
+        <Button variant={"ghost"} asChild>
+          <a href="/profile">
+            <FaUserCircle />
+          </a>
+        </Button>
+      </Box>
+    </Box>
+  );
+}
