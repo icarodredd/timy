3199e097a8b1e077fdd8a90c95f86924b173cc05
icarodredd <icarog854@gmail.com>
2025-09-20 14:42:26 -0300

    build: configure remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      new URL(process.env.NEXT_PUBLIC_SUPABASE_URL + "/**" || ""),
+    ],
+  },
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
