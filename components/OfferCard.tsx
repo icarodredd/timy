8ce310bdd018ea71773692d73681f269dc47d2e8
icarodredd <icarog854@gmail.com>
2025-09-27 10:29:34 -0300

    feat: productCard

---
+++ components/OfferCard.tsx
@@ -0,0 +1,37 @@
+import { Offer } from "@/app/page";
+import { Button, Card, Image, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function OfferCard({ offer }: { offer: Offer }) {
+  return (
+    <Link className="w-full h-full" href={"#"}>
+      <Card.Root maxW="sm" height={"100%"} border={"none"} overflow="hidden">
+        <Image
+          backgroundColor={"white"}
+          padding={"40px"}
+          src={offer.image_url}
+          alt={offer.title}
+        />
+        <Card.Body gap="2">
+          <Card.Title>{offer.title}</Card.Title>
+          <Card.Description>{offer.description}</Card.Description>
+          <Text
+            textStyle="2xl"
+            fontWeight="medium"
+            letterSpacing="tight"
+            mt="2"
+          >
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+            }).format(offer.price)}
+          </Text>
+        </Card.Body>
+        <Card.Footer gap="2">
+          <Button variant="solid">Buy now</Button>
+          <Button variant="ghost">Add to cart</Button>
+        </Card.Footer>
+      </Card.Root>
+    </Link>
+  );
+}
