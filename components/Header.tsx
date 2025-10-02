51acfd72a165e5a8bbbbe81adcdc8a20781f959c
icarodredd <icarog854@gmail.com>
2025-10-02 16:38:12 -0300

    feat: header link to homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,7 @@ import NextLink from "next/link";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaUserCircle } from "react-icons/fa";
 import useThemeDetector from "@/app/hooks/useThemeDetector";
+import Link from "next/link";
 
 export default function Header() {
   const isDarkMode = useThemeDetector();
@@ -25,13 +26,16 @@ export default function Header() {
       py={2}
       px={6}
     >
-      <Image
-        src={isDarkMode ? whiteTimyLogo : darkTimyLogo}
-        alt="Timy Logo"
-        width={200}
-        height={200}
-        style={{ width: "30vw", height: "auto", maxWidth: "200px" }}
-      />
+      <Link href={"/"}>
+        <Image
+          src={isDarkMode ? whiteTimyLogo : darkTimyLogo}
+          alt="Timy Logo"
+          width={200}
+          height={200}
+          style={{ width: "30vw", height: "auto", maxWidth: "200px" }}
+        />
+      </Link>
+
       <Box display={{ mdDown: "none", md: "flex" }} gap={10}>
         <ChakraLink asChild>
           <NextLink href="/best-offers">Best Offers</NextLink>
