c88db6ddb22b15d68f7c538fba9292388eb2a2ff
icarodredd <icarog854@gmail.com>
2025-10-03 17:00:48 -0300

    feat: add menu on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,14 @@
 import Image from "next/image";
 import whiteTimyLogo from "@/public/timy-logo-white.png";
 import darkTimyLogo from "@/public/timy-logo-dark.png";
-import { Box, Button, Link as ChakraLink } from "@chakra-ui/react";
+import { Box, Link as ChakraLink } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaUserCircle } from "react-icons/fa";
 import useThemeDetector from "@/app/hooks/useThemeDetector";
 import Link from "next/link";
+import { Button, Menu, Portal } from "@chakra-ui/react";
+import { IoMenu } from "react-icons/io5";
 
 export default function Header() {
   const isDarkMode = useThemeDetector();
@@ -23,19 +25,48 @@ export default function Header() {
       as={"header"}
       display={"flex"}
       justifyContent={"space-between"}
+      alignItems={"center"}
       py={2}
-      px={6}
+      px={2}
     >
-      <Link href={"/"}>
-        <Image
-          src={isDarkMode ? whiteTimyLogo : darkTimyLogo}
-          alt="Timy Logo"
-          width={200}
-          height={200}
-          style={{ width: "30vw", height: "auto", maxWidth: "200px" }}
-        />
-      </Link>
-
+      <Box display={"flex"} alignItems={"center"}>
+        <Box display={{ mdDown: "flex", md: "none" }}>
+          <Menu.Root>
+            <Menu.Trigger asChild>
+              <Button variant="ghost" size="md">
+                <IoMenu />
+              </Button>
+            </Menu.Trigger>
+            <Portal>
+              <Menu.Positioner>
+                <Menu.Content>
+                  <Menu.Item asChild value="best-offers">
+                    <Link href={"/best-offers"}>Best Offers</Link>
+                  </Menu.Item>
+                  <Menu.Item asChild value="all-offers">
+                    <Link href={"/all-offers"}>All Offers</Link>
+                  </Menu.Item>
+                  <Menu.Item asChild value="Feminine">
+                    <Link href={"/feminine"}>Feminine</Link>
+                  </Menu.Item>
+                  <Menu.Item asChild value="masculine">
+                    <Link href={"/masculine"}>Masculine</Link>
+                  </Menu.Item>
+                </Menu.Content>
+              </Menu.Positioner>
+            </Portal>
+          </Menu.Root>
+        </Box>
+        <Link href={"/"}>
+          <Image
+            src={isDarkMode ? whiteTimyLogo : darkTimyLogo}
+            alt="Timy Logo"
+            width={200}
+            height={200}
+            style={{ width: "30vw", height: "auto", maxWidth: "200px" }}
+          />
+        </Link>
+      </Box>
       <Box display={{ mdDown: "none", md: "flex" }} gap={10}>
         <ChakraLink asChild>
           <NextLink href="/best-offers">Best Offers</NextLink>
