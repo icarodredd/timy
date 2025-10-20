3d12db82bd52675ce914c2f328f35b1525452ab7
icarodredd <icarog854@gmail.com>
2025-10-20 17:26:21 -0300

    feat: feminine department

---
+++ app/feminine/page.tsx
@@ -0,0 +1,76 @@
+import OfferCard from "@/components/OfferCard";
+import EmptyMessage from "@/components/ui/EmptyMessage";
+import RangePriceFilter from "@/components/ui/RangePriceFilter";
+import { createClient } from "@/utils/supabase/server";
+import { Box, Breadcrumb, Grid, Heading } from "@chakra-ui/react";
+import { Offer } from "../page";
+
+type Search = {
+  minPrice?: string;
+  maxPrice?: string;
+};
+
+export default async function FemininePage({
+  searchParams,
+}: {
+  searchParams: Search;
+}) {
+  const supabase = await createClient();
+  const minPrice = Number(searchParams?.minPrice) || 0;
+  const maxPrice = Number(searchParams?.maxPrice) || 50000;
+
+  const { data } = await supabase
+    .from("all_offers")
+    .select()
+    .eq("gender", "feminine")
+    .gte("price", minPrice)
+    .lte("price", maxPrice);
+
+  const feminineOffers = data as Offer[];
+
+  return (
+    <main style={{ paddingTop: "18vh" }}>
+      <Breadcrumb.Root size={"lg"} my={2}>
+        <Breadcrumb.List>
+          <Breadcrumb.Item>
+            <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Separator />
+          <Breadcrumb.Item>
+            <Breadcrumb.Link href="/feminine">
+              Feminine Department
+            </Breadcrumb.Link>
+          </Breadcrumb.Item>
+        </Breadcrumb.List>
+      </Breadcrumb.Root>
+      <hr />
+      <Box
+        as={"section"}
+        display={"flex"}
+        width={"full"}
+        justifyContent={"space-between"}
+      >
+        <Box display={"flex"} flexDirection={"column"} width={"15%"}>
+          <Heading textAlign={"start"} fontWeight={"bold"} size={"xl"} py={4}>
+            Filters
+          </Heading>
+          <RangePriceFilter minPrice={minPrice || 0} maxPrice={maxPrice || 0} />
+        </Box>
+        <Box display={"flex"} flexDirection={"column"} width={"80%"}>
+          <Heading textAlign={"start"} fontWeight={"bold"} size={"4xl"} py={12}>
+            Feminine Department
+          </Heading>
+          {feminineOffers.length > 0 ? (
+            feminineOffers?.map((offer) => (
+              <Grid templateColumns="repeat(4, 1fr)" gap="6">
+                <OfferCard key={offer.id} offer={offer} />
+              </Grid>
+            ))
+          ) : (
+            <EmptyMessage />
+          )}
+        </Box>
+      </Box>
+    </main>
+  );
+}
